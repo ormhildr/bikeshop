733e9e1f6aada5afb639daf81e20582809667756
Dmitry <bigvolkovski@gmail.com>
2021-02-27 18:01:07 +0300

    Различные правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const element = document.getElementById("user-phone");
+const maskOptions = {
+  mask: "+{7}(000)000-00-00"
+};
+const mask = IMask(element, maskOptions);
+
+const body = document.body;
 const navMain = document.querySelector(".main-nav");
 const navToggle = navMain.querySelector(".main-nav__toggle");
 
@@ -11,5 +18,11 @@ if (navMain !== null) {
   navToggle.addEventListener("click", function () {
     navMain.classList.toggle("main-nav--closed");
     navMain.classList.toggle("main-nav--opened");
+    if (navMain.classList.contains("main-nav--opened")) {
+      body.style.overflow = "hidden";
+    }
+    if (navMain.classList.contains("main-nav--closed")) {
+      body.style.overflow = "visible";
+    }
   });
 }
